97ec38592243a7713d818f0f8e8c6f5630908b82
aweholy777 <aweholy@gmail.com>
2026-06-16 12:41:38 +0800

    清理流程：刪除 3 支過時模型安裝/下載 recipe（download_lora/download_clipvision/setup_infinitetalk，路徑失效，需要時重寫）

--- video-pipeline/download_clipvision.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set LOG=C:\Users\aweholy\Desktop\clone2026010\aweholy777.github.io\video-pipeline\_clipvision_log.txt
-set MODELS=C:\Users\aweholy\ComfyUI-Shared\models
-echo clip_vision_h download start %time% > "%LOG%"
-curl -L -sS -C - --retry 5 -o "%MODELS%\clip_vision\clip_vision_h.safetensors" "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors" >> "%LOG%" 2>&1
-echo CLIPVISION_DONE %time% >> "%LOG%"
